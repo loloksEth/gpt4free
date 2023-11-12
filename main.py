12a6207f908eb4efc4c7f52f0e51d209e84ff3bf
kostya <silirauk11@mail.ru>
2023-11-12 04:11:40 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,27 @@
+from typing import List
+
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+
+@app.post("/files/")
+async def create_files(files: List[bytes] = File(default=...)):
+    return {"file_sizes": [len(file) for file in files]}
+
+
+
+
+
+
+@app.get("/")
+async def main():
+    content = """
+<body>
+<form action="/files/" enctype="multipart/form-data" method="post">
+<input name="files" type="file" multiple>
+<input type="submit">
+</body>
+    """
+    return HTMLResponse(content=content)
---
+++ pytest.py
@@ -0,0 +1,5 @@
+def avg(avg):
+    assert len(avg)!=0
+    return sum(avg)/len(avg)
+
+print(avg([]))
